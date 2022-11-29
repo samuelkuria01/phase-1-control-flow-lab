af08d8b4977bf22a90b5fe3c8b0e19ce21a449f3
samuelkuria01 <samuel.kuria01@student.moringaschool.com>
2022-11-29 13:35:04 +0300

    trial and error

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(Feet){
+  if(Feet<=400){
+    return('Thats on me');
+
+  }else if(Feet>2000){
+    return('I will gladly take your thirty bucks');
+  }
+  else (Feet>2500)
+    return('No can do');
+  }
+
+function ternaryCheckCity(city){
+  if (city=NYC) {
+    return('Ok sounds good');
+    
+  } else {
+    return('No go');
+    
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+// Write your code here!
+const tip = prompt('entre tip');
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  if (tip='generous')
+return ("Thank you so much");
+
+}else if (tip='not generous'){
+  return "Thank you ";
+
+}else {
+return ('thanks for everything .Bye')
 }
